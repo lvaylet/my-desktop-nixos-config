7e0bbf260e593e8ed60725f160b70152c9a30bc8
Laurent VAYLET <laurent.vaylet@gmail.com>
2026-05-03 20:39:58 +0200

    feat: tune Visual Studio Code user settings

--- home.nix
+++ home.nix
@@ -147,10 +147,24 @@
         userSettings = {
           # This property will be used to generate `settings.json`:
           # https://code.visualstudio.com/docs/getstarted/settings#_settingsjson
+          "editor.fontFamily" = "MesloLGM Nerd Font";
+          "editor.fontLigatures" = true;
+          "editor.lineNumbers" = "relative";
           "editor.fontSize" = 14;
           "editor.formatOnSave" = true;
+          "editor.rulers" = [
+            80
+            88
+            100
+            120
+          ];
 
+          "diffEditor.codeLens" = true;
           "diffEditor.hideUnchangedRegions.enabled" = true;
+          "diffEditor.ignoreTrimWhitespace" = false;
+
+          "explorer.confirmDelete" = false;
+          "explorer.confirmDragAndDrop" = false;
 
           "files.associations" = {
             "*.bu" = "yaml";
@@ -158,26 +172,42 @@
             "*.ign" = "json";
           };
           "files.autoSave" = "afterDelay";
+          "files.insertFinalNewline" = true;
+          "files.trimFinalNewlines" = true;
 
           "git.autofetch" = true;
           "git.confirmSync" = false;
+          "git.suggestSmartCommit" = false;
 
           "terminal.integrated.fontFamily" = "MesloLGM Nerd Font";
           "terminal.integrated.fontLigatures.enabled" = true;
+          "terminal.integrated.fontSize" = 12;
+          "terminal.integrated.lineHeight" = 1.2;
 
+          "window.menuBarVisibility" = "toggle";
+
+          # Language Specific Editor Settings
+          # ---
+          # Source: https://code.visualstudio.com/docs/configure/settings#_language-specific-editor-settings
+          
           # Nix
+          # ---
+          # Easiest Way To Write Nix | Code Editor Setup, by Vimjoyer
+          # https://www.youtube.com/watch?v=M_zMoHlbZBY&t=262s
+          # https://github.com/vimjoyer/nix-editor-setup-video?tab=readme-ov-file#vscode-nix-ide-setup
           "[nix]" = {
             "editor.tabSize" = 2;
-            "editor.defaultFormatter" = "kamadorueda.alejandra";
             "editor.formatOnPaste" = true;
             "editor.formatOnSave" = true;
             "editor.formatOnType" = false;
           };
-          "alejandra.program" = "alejandra";
           "nix.enableLanguageServer" = true; # Enable LSP.
-          # "nix.formatterPath" = "alejandra"; # "nixfmt", "alejandra"
-          "nix.serverPath" = "nil"; # The path to the LSP server executable: "nil", "nixd", or ["executable", "argument1", ...]
-          # "nix.serverSettings.nil.formatting.command" = [ "nixfmt" ];
+          "nix.serverPath" = "nixd"; # The path to the LSP server executable: "nil", "nixd", or ["executable", "argument1", ...]
+          "nix.serverSettings" = {
+            "nixd" = {
+              "formatting.command" = [ "alejandra" ]; # nixfmt, nixpkgs-fmt, alejandra
+            };
+          };
         };
       };
     };
